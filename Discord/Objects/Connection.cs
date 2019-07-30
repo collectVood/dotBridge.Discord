0f2cc83f8713d775f00c11f3e19a8b2911879471
collect_vood <yannischu123@gmail.com>
2019-07-30 16:02:25 +0200

    :cookie: Add VoiceRegion, InviteMetadata, Connection object

---
+++ Discord/Objects/Connection.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace dotBridge.Discord.Objects
+{
+    public class Connection
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string ID;
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name;
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type;
+
+        [JsonProperty(PropertyName = "revoked")]
+        public bool revoked;
+
+        [JsonProperty(PropertyName = "integrations")]
+        public Integration[] Integrations;
+
+        [JsonProperty(PropertyName = "verified")]
+        public bool Verified;
+
+        [JsonProperty(PropertyName = "friend_sync")]
+        public bool FriendSync;
+
+        [JsonProperty(PropertyName = "show_activity")]
+        public bool ShowActivity;
+
+        [JsonProperty(PropertyName = "visibility")]
+        public int Visibility;
+    }
+}
